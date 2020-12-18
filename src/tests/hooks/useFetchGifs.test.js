671f232e18e30e501f9cd9b41d53d4beb080647c
Juan33 <58494935+juansolares333@users.noreply.github.com>
2020-12-18 17:12:04 -0600

    commit

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,17 +2,27 @@ import { renderHook } from "@testing-library/react-hooks"
 import { useFetchGifs } from "../../hooks/useFetchGifs"
 
 describe('tests on the hook useFetchGifs', () => {
-    
-    test('should return the initial state', () => {
-        
-        //const { data, loading } = useFetchGifs('Nintendo');
-        const {result} = renderHook(() => useFetchGifs('Nintendo'));
-        const { data, loading } = result.current;
 
+    test('should return the initial state', async () => {
+        
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Nintendo'));
+        const { data, loading } = result.current;
+        await waitForNextUpdate();
         expect(data).toEqual([]);
         expect(loading).toBe(true);
 
-    })
-    
+    });
+
+
+    test('should return an array of images and loading: false', async () => {
+
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Nintendo'));
+        const { data, loading } = result.current;
+        await waitForNextUpdate();
+        expect(data).toEqual([]);
+        expect(loading).toBe(true);
+    });
+
+
 
 })
